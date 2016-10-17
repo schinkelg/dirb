3d74908cbbfde8df1cccf244f674a46b27f1d5d1
cedriczirtacic <cicatriz.r00t@gmail.com>
2016-10-16 23:00:57 -0600

    Cleaned up clean_wordlist.sh for *nix use

--- utils/clean_wordlist.sh
+++ utils/clean_wordlist.sh
@@ -1,15 +1,10 @@
 #!/bin/sh
-#
-#
-
+if [ $# -lt 1 ];then
+  echo "$0 <wordlist>" && exit 1
+fi
 echo "Cleaning $1"
+cp $1 $1.old
+sort -d $1 | uniq > $1.new
+mv $1.new $1
+printf "Number of words: %s\n" `wc -l $1|awk '{print $1}'`
 
-cat $1 > $1.old
-
-sort $1 > tmp.1
-
-uniq.exe tmp.1 > tmp.2
-
-cat tmp.2 > $1
-
-rm tmp.*
